248b1d877f74f7fa84ccd7605aaffe565d773d16
Joey Killam <jhkillam@gmail.com>
2020-03-24 21:41:29 -0500

    testing video

--- public/app.js
+++ public/app.js
@@ -38,7 +38,41 @@ async function createRoom() {
   registerPeerConnectionListeners();
 
   // Add code for creating a room here
-  
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+
+  const roomWithOffer = {
+      offer: {
+          type: offer.type,
+          sdp: offer.sdp
+      }
+  }
+  const roomRef = await db.collection('rooms').add(roomWithOffer);
+  const roomId = roomRef.id;
+  document.querySelector('#currentRoom').innerText = `Current room is ${roomId} - You are the caller!`
+
+  roomRef.onSnapshot(async snapshot -> {
+    console.log('Got updated room:', snapshot.data());
+    const data = snapshot.data();
+    if (!peerConnection.currentRemoteDescription && data.answer) {
+        console.log('Set remote description: ', data.answer);
+        const answer = new RTCSessionDescription(data.answer)
+        await peerConnection.setRemoteDescription(answer);
+    }
+});
+
+  const offer = roomSnapshot.data().offer;
+  await peerConnection.setRemoteDescription(offer);
+  const answer = await peerConnection.createAnswer();
+  await peerConnection.setLocalDescription(answer);
+
+  const roomWithAnswer = {
+      answer: {
+          type: answer.type,
+          sdp: answer.sdp
+      }
+  }
+  await roomRef.update(roomWithAnswer);
   // Code for creating room above
   
   localStream.getTracks().forEach(track => {
@@ -50,7 +84,26 @@ async function createRoom() {
   // Code for creating a room above
 
   // Code for collecting ICE candidates below
+  async function collectIceCandidates(roomRef, peerConneciton,    
+    localName, remoteName) {
+const candidatesCollection = roomRef.collection(localName);
 
+peerConnection.addEventListener('icecandidate', event -> {
+if (event.candidate) {
+const json = event.candidate.toJSON();
+candidatesCollection.add(json);
+}
+});
+
+roomRef.collection(remoteName).onSnapshot(snapshot -> {
+snapshot.docChanges().forEach(change -> {
+if (change.type === "added") {
+const candidate = new RTCIceCandidate(change.doc.data());
+peerConneciton.addIceCandidate(candidate);
+}
+});
+})
+}
   // Code for collecting ICE candidates above
 
   peerConnection.addEventListener('track', event => {
